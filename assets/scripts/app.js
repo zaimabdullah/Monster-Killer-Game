fbf90dac24416a8dc30df74a9e347e46efd271b4
Husni Zaim <zcreative14@gmail.com>
2022-11-14 15:44:40 +0800

    Update if-statement to switch-case in writeToLog func

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -33,15 +33,34 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
     finalMonsterHealth: monsterHealth,
     finalPlayerHealth: playerHealth,
   };
-  if (ev === LOG_EVENT_PLAYER_ATTACK) {
-    logEntry.target = 'MONSTER';
-  } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-    logEntry.target = 'MONSTER';
-  } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
-    logEntry.target = 'PLAYER';
-  } else if (ev === LOG_EVENT_PLAYER_HEAL) {
-    logEntry.target = 'PLAYER';
+
+  switch (ev) {
+    case LOG_EVENT_PLAYER_ATTACK:
+      logEntry.target = 'MONSTER';
+      break;
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry.target = 'MONSTER';
+      break;
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry.target = 'PLAYER';
+      break;
+    case LOG_EVENT_PLAYER_HEAL:
+      logEntry.target = 'PLAYER';
+      break;
+    default:
+      logEntry = {};
+      break;
   }
+
+  // if (ev === LOG_EVENT_PLAYER_ATTACK) {
+  //   logEntry.target = 'MONSTER';
+  // } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+  //   logEntry.target = 'MONSTER';
+  // } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
+  //   logEntry.target = 'PLAYER';
+  // } else if (ev === LOG_EVENT_PLAYER_HEAL) {
+  //   logEntry.target = 'PLAYER';
+  // }
   // else if (ev === LOG_EVENT_GAME_OVER) {
   //   logEntry = {
   //     event: ev,
