0ca9488283a5aeafaf0288c761903c214f96e77c
Husni Zaim <zcreative14@gmail.com>
2022-11-25 20:09:17 +0800

    Add try-catch error handling

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,14 +11,28 @@ const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
 const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
 const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 
-const enteredValue = prompt('Maximum life for you and the monster.', '100');
-
-let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
 let lastLoggedEntry;
 
-if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+function getMaxLifeValues() {
+  const enteredValue = prompt('Maximum life for you and the monster.', '100');
+
+  const parsedValue = parseInt(enteredValue);
+  if (isNaN(parsedValue) || parsedValue <= 0) {
+    throw { message: 'Invalid user input, not a number!' };
+  }
+  return parsedValue;
+}
+
+let chosenMaxLife;
+
+try {
+  chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+  console.log(error);
   chosenMaxLife = 100;
+  alert('You entered something wrong, default value of 100 was used.');
+  // throw error;
 }
 
 let currentMonsterHealth = chosenMaxLife;
@@ -200,7 +214,7 @@ function printLogHandler() {
   //   }
   //   j++;
   // } while (j < 3);
-
+  let i = 0;
   for (const logEntry of battleLog) {
     // for-of loop
     if (!lastLoggedEntry && lastLoggedEntry !== 0 || lastLoggedEntry < i) {
